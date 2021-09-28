ec9df9707609a74277e0d7787a60eed0ce0aee0e
JoaoTony <joaojosetony2016@gmail.com>
2021-09-28 16:49:19 +0100

    feat: add home component

---
+++ src/views/home/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+import styles from "./home.styles";
+import { View } from 'react-native'
+
+const Home: FC = () => {
+  return (
+    <View style={styles.container}>
+    </View>
+  )
+}
+
+export default Home
